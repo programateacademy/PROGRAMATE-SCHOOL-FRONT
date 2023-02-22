febfef77c86b5851d33dc966860c216f3bc25fcd
Lily Johana <lilajoha2907@gmail.com>
2023-02-22 00:35:37 -0500

    init lily

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const Header = () => {
     return (
-        <div className=''>
+        <div className=' bg-purple'>
             Header Lily
         </div>
     )
