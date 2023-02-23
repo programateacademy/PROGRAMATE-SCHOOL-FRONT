7c800c2dc1f93ac12256e7230d30e3c59a2fb2c6
Lily Johana <lilajoha2907@gmail.com>
2023-02-23 15:58:21 -0500

    v-2 header and v4- nav

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import { TbTriangles } from "react-icons/tb";
 
 export const Header = () => {
     return (
-        <div className=' bg-purple'>
-            Header Lily
+        // image 
+        <div className='container bg-gradient-to-b from-slate-300 via-black to-neutral-900 h-full  '>
+            <div className='flex flex-col  p-2 pt-56 sm: md: lg: xl: 2xl: '>
+                <h3 className=' font-Nunito text-light text-xl text-center sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl  '>Convocatoria virtual</h3>
+                <h1 className='font-Poppins font-bold  text-light text-4xl text-center sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl  '>Aprende a desarrollar aplicaciones Web</h1>
+                <TbTriangles className='text-yellow rotate-180 text-6xl m-auto my-2 sm:text-7xl md:text-8xl lg:text-9xl ' />
+            </div>
+            
+            {/* <div className='w-0 h-0 border-t-[100px] border-l-[50px] border-r-[50px] border-r-transparent border-l-transparent border-yellow'></div> */}
         </div>
     )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,36 +8,40 @@ export const Navbar = () => {
 
     return (
         <div className='container'>
-            {/* //logo Programate school */}
-            <div className=' fixed flex flex-row  p-4 items-center justify-around'>
-                <div className=' w-40 h-14 bg-orange-600 '> logo school</div>
-
-                {/* //menu responsive */}
-                <div className=' invisible sm:visible gap-5 font-Poppins'>
-                    {/* <Link to=""> */}
-                        <h1 className='' alt="Convocatorias">Convocatorias</h1>
-                    {/* </Link>
-                    <Link to=""> */}
-                        <h1 className='' alt="Contáctanos">Contáctanos</h1>
-                    {/* </Link> */}
-                </div>
-
-                {/* //menu hamburger */}
-                <HiMenu
-                    className='sm:hidden flex flex-row text-yellow text-3xl justify-items-end'
-                    onClick={() => setIsOpen(!isOpen)}
-                />
-                {isOpen === true ? (
-                    <div className=' bg-light rounded-sm p-3 flex flex-col font-Poppins '>
+            
+            <div className='w-full fixed flex flex-row  p-4 items-center justify-between mx-auto '>
+                {/* //logo Programate school */}
+                <div className=' w-40 h-14 bg-orange-600 sm:w-44 sm:h-16 lg:w-56 lg:h-24'> logo school</div>
+                <div className='flex flex-row'>
+                    {/* //menu responsive */}
+                    <div className=' fixed px-12 text-lg font-Poppins flex invisible sm:visible sm:text-sm md:text-base lg:text-lg
+                    xl:text-xl
+                    2xl:text-2xl'>
                         {/* <Link to=""> */}
-                            <h1 className='' alt="Convocatorias">Convocatorias</h1>
+                            <h1 className='px-3' alt="Convocatorias">Convocatorias</h1>
                         {/* </Link>
                         <Link to=""> */}
-                            <h1 className='' alt="Contáctanos">Contáctanos</h1>
+                            <h1 className='px-3' alt="Contáctanos">Contáctanos</h1>
                         {/* </Link> */}
                     </div>
-                ) : null
-                }
+
+                    {/* //menu hamburger */}
+                    <HiMenu
+                        className=' sm:hidden text-yellow text-3xl my-2'
+                        onClick={() => setIsOpen(!isOpen)}
+                    />
+                    {isOpen === true ? (
+                        <div className=' bg-yellow rounded-lg p-3 flex flex-col font-Poppins fixed mt-10 right-5'>
+                            {/* <Link to=""> */}
+                            <button className='focus:bg-gray-200 rounded-lg p-3' alt="Convocatorias">Convocatorias</button>
+                            {/* </Link>
+                            <Link to=""> */}
+                            <button className='focus:bg-gray-100 rounded-lg p-3' alt="Contáctanos">Contáctanos</button>
+                            {/* </Link> */}
+                        </div>
+                    ) : null
+                    }
+                </div>    
             </div>
         </div>
     )
