2882e91291d8274d95312d775ce5267c0e8c3e07
MichiCmc <stepaanymicor@gmail.com>
2023-02-22 22:36:56 -0500

    Inicio del footer frase de programate

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,9 +2,13 @@ import React from 'react'
 
 export const Footer = () => {
     return (
-        <div className=''>
-            Footer Michell
+     <>
+     <footer>
+        <div className='flex justify-center items-center bg-yellow p-4 w-'>
+            <h1 className='text-xs'>La tecnología esta revolucionando nuestro presente, pero tu eres nuestro futuro</h1>
         </div>
+     </footer>
+     </>
     )
 }
 
