f98a8db5600cc70be0055d87534addb5b5db42f2
danilodiaz <diazdanilo340@gmail.com>
2023-02-28 18:25:34 -0500

    progress StudenSpace

--- src/pages/StudentSpace.jsx
+++ src/pages/StudentSpace.jsx
@@ -3,10 +3,31 @@ import Call from '../components/StudentSpace/CallComp'
 
 const StudentSpace = () => {
     return (
-        <div>
-            StudentSpace
-            <Call/>
-        </div>
+        <main className='container py-10'>
+            {/* session convocations */}
+            <section className='flex justify-around'>
+
+                <h1 className='font-bold font-Poppins'>Convocatorias abiertas</h1>
+
+                <figure className='flex flex-col justify-end items-center'>
+                    <h3 className='font-Poppins font-semibold'>¿Tienes algún problema o dificultad con tu proceso?</h3>
+                    
+                    <p className=' font-Nunito py-4'>No dude en contáctarnos estaremos pendiente a darte una pronta solución</p>
+                    <button className='bg-yellow shadow-md shadow-dark/50 hover:bg-dark w-32 h-8 rounded text-center text-light font-Poppins font-semibold'>Contáctanos</button>
+
+                    <hr className='border-2 rounded border-yellow w-48' />
+                    <p>Agradecemos a nuestros sponsor por brindar apoyo a este programa</p>
+                    <img className=''
+                        src={
+                            "https://github.com/MariaHerrera03/ImageBank/blob/main/Progr%C3%A1mateSchool/brillas-color.png?raw=true"
+                        }
+                        alt="logo-brillas"
+                    />
+
+                </figure>
+            </section>
+            <Call />
+        </main>
     )
 }
 
