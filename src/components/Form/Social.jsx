10e623b13b48a6cc7aae59e703eda15a2d254240
Sofia Sanchez <ssofiiadayanna@gmail.com>
2023-02-28 20:18:20 -0500

    bring changes

--- src/components/Form/Social.jsx
+++ src/components/Form/Social.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 const Social = () => {
     return (
-        <div>Social</div>
+        <div className='h-full'>
+        <div className='row-span-1 bg-[url("https://github.com/MariaHerrera03/ImageBank/blob/main/Progr%C3%A1mateSchool/PhotoOCHO_50.png?raw=true")] bg-cover bg-center grid'>
+            <div className='w-7/12 sm:w-5/12 md:w-9/12 m-auto'>
+                <img className='bg-light/80 rounded-lg p-2 md:p-4 shadow-2xl' src={'https://github.com/MariaHerrera03/ImageBank/blob/main/Progr%C3%A1mateSchool/programate-school-color.png?raw=true'}/>
+            </div>
+        </div>
+   
+
+            </div>
+
     )
 }
 
--- src/components/Form/Student.jsx
+++ src/components/Form/Student.jsx
@@ -3,7 +3,7 @@ import Inlet from './Inlet'
 
 const Student = () => {
     return (
-        <div>Student
+        <div>yo
             <Inlet />
         </div>
     )
