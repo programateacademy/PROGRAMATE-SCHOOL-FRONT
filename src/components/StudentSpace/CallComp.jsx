d28efea55cecadc88913d537f9bc50747a8669cd
danilodiaz <diazdanilo340@gmail.com>
2023-03-02 17:37:33 -0500

    process responsive

--- src/components/StudentSpace/CallComp.jsx
+++ src/components/StudentSpace/CallComp.jsx
@@ -1,35 +1,40 @@
 import React from 'react'
 
 const CallComp = () => {
-    function fillId(id) {
-        const numerator = 0;
-        const denominator = 6;
-        const fraction = numerator + '/' + denominator;
-        document.getElementById(id).innerHTML = fraction;
 
-        const interval = setInterval(function () {
-            numerator++;
-            if (numerator > denominator) {
-                clearInterval(interval);
-            } else {
-                fraction = numerator + '/' + denominator;
-                document.getElementById(id).innerHTML = fraction;
-            }
-        }, 1000); // delay between iterations (in milliseconds)
-    }
+    // function fillId(id) {
 
-    fillId('Numbers');
+    //     const numerator = 0;
+    //     const denominator = 6;
+    //     const fraction = numerator + '/' + denominator;
+
+    //     document.getElementById(id).innerHTML = fraction;
+
+    //     const interval = setInterval(function () {
+
+    //         numerator++;
+    //         if (numerator > denominator) {
+    //             clearInterval(interval);
+    //         } else {
+    //             fraction = numerator + '/' + denominator;
+    //             document.getElementById(id).innerHTML = fraction;
+    //         }
+
+    //     }, 1000);
+    // }
+
+    // fillId('Numbers');
 
     return (
         <main className='shadow shadow-yellow/50 rounded p-3 md:w-5/12 lg:w-4/12'>
-            <section className='flex flex-col items-center '>
+            <section className='grid grid-cols-1'>
                 <h1 className='font-bold font-Poppins my-4'>Desarrollador Frontend Web Jr</h1>
-                <figure className='flex flex-col justify-around '>
+                <figure className='flex flex-col justify-around items-center'>
                     <p className='font-nonito my-4'>Estado de la aplicación <span className='text-dark font-bold ml-10'>En progreso</span></p>
-                    <p className='font-nonito my-4'>Etapas de la selección <span id='Numbers' className='text-dark font-bold ml-20'>2/6</span></p>
+                    <p className='font-nonito my-4 row-start-3'>Etapas de la selección <span id='Numbers' className='text-dark font-bold ml-20'>2/6</span></p>
+                <hr className='border-1 rounded border-yellow w-full my-10' />
                 </figure>
-                <hr className='border-1 rounded border-yellow w-48 my-10' />
-                <figure className='flex flex-col items-center'>
+                <figure className='flex flex-col justify-center items-center'>
                     <p className='font-nonito my-4'>Proceso de selección <span className='text-dark font-bold ml-16'>Activo</span></p>
                     <button className='bg-yellow shadow-md shadow-dark/50 hover:bg-dark w-32 h-8 rounded text-center text-light font-Poppins font-semibold my-6'>Continuar</button>
                 </figure>
--- src/pages/StudentSpace.jsx
+++ src/pages/StudentSpace.jsx
@@ -7,26 +7,27 @@ const StudentSpace = () => {
 
             {/* session convocations */}
 
-            <section className='flex flex-col items-center'>
-                <h1 className='font-bold font-Poppins text-md md:text-lg lg:text-3xl'>Convocatorias abiertas</h1>
+            <section className='flex'>
+                <h1 className='font-bold font-Poppins text-md md:text-lg lg:text-3xl lg:col-start-1 py-10'>Convocatorias abiertas</h1>
                 <Call />
 
-                <figure className='flex flex-col items-center'>
-                    <h3 className='font-Poppins font-semibold m-2 mt-10 text-sm'>¿Tienes algún problema o dificultad con tu proceso?</h3>
-                    <p className=' font-Nunito m-2'>No dude en contáctarnos estaremos pendiente a darte una pronta solución</p>
-                    <button className='bg-yellow shadow-md shadow-dark/50 hover:bg-dark w-32 h-8 rounded text-center text-light font-Poppins font-semibold my-8'>Contáctanos</button>
-                </figure>
+                    <figure className='col-start-2 flex flex-col justify-center items-center '>
+                        <h3 className='font-Poppins font-semibold m-2 mt-10 text-sm'>¿Tienes algún problema o dificultad con tu proceso?</h3>
+                        <p className=' font-Nunito m-2'>No dude en contáctarnos estaremos pendiente a darte una pronta solución</p>
+                        <button className='bg-yellow shadow-md shadow-dark/50 hover:bg-dark w-32 h-8 rounded text-center text-light font-Poppins font-semibold my-8'>Contáctanos</button>
+                    </figure>
 
-                <figure className='flex flex-col items-center'>
-                    <hr className='border-2 rounded border-yellow w-60 my-4' />
-                    <p className='font-Nunito m-3'>Agradecemos a nuestros sponsor por brindar apoyo a este programa</p>
-                    <img className='my-10'
-                        src={
-                            "https://github.com/MariaHerrera03/ImageBank/blob/main/Progr%C3%A1mateSchool/brillas-color.png?raw=true"
-                        }
-                        alt="logo-brillas"
-                    />
-                </figure>
+                    <figure className='flex flex-col items-center'>
+                        <hr className='border-2 rounded border-yellow w-full my-4' />
+                        <p className='font-Nunito m-3'>Agradecemos a nuestros sponsor por brindar apoyo a este programa</p>
+                        <img className='my-10'
+                            src={
+                                "https://github.com/MariaHerrera03/ImageBank/blob/main/Progr%C3%A1mateSchool/brillas-color.png?raw=true"
+                            }
+                            alt="logo-brillas"
+                        />
+                    </figure>
+                
             </section>
 
 
