256eb2c17196e808f4c015e937dc3d0e2edacc8d
María F. Herrera G <mariaf.herrera@outlook.com>
2023-03-02 01:12:55 -0500

    V0.14 BackEnd updates and code reviews

--- src/components/Home/Navbar.jsx
+++ src/components/Home/Navbar.jsx
@@ -8,7 +8,7 @@ export const Navbar = () => {
 
     return (
         <nav className=''>
-            <section className='w-full absolute z-40 flex flex-row md:grid md:grid-cols-5 p-4 items-center justify-between m-auto'>
+            <section className='absolute z-40 flex flex-row md:grid md:grid-cols-5 px-4 py-2 items-center justify-between m-auto md:mx-14 md:mt-3'>
                 
                 {/* Logo Programate school */}
                 <img src={'https://github.com/MariaHerrera03/ImageBank/blob/main/Progr%C3%A1mateSchool/programate-school-negros.png?raw=true'} className=' w-2/6 flex md:col-span-1 md:w-full'></img>
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -40,11 +40,11 @@ function LoginPerson() {
                 <h1 className='mx-12 sm:mx-12 md:mx-12 lg:mx-26 pb-3 text-center text-yellow text-lg sm:text-2xl md:text-2xl lg:text-3xl font-Poppins font-black uppercase'>¡Sigue educándote<br/>para el futuro!</h1>
                 <Link to='/Singup'><img className='mx-auto w-[25%] pb-6 md:hidden' src={'https://github.com/MariaHerrera03/ImageBank/blob/main/Progr%C3%A1mateSchool/programate-school-negros.png?raw=true'} alt=''/></Link>
                 <div className='mx-12 sm:mx-40 md:mx-16 lg:mx-28 pb-6'>
-                    <label for='email' className='pb-1.5 text-dark text-sm font-Nunito font-black'>Usuario:</label>
+                    <label className='pb-1.5 text-dark text-sm font-Nunito font-black'>Usuario:</label>
                     <input value={emailPerson} onChange={(e) => {setEmailPerson(e.target.value)}} type='email' required  placeholder='correo@correo.edu.co' className='w-full px-1 py-1 rounded border-2 border-yellow text-dark/50 text-xs font-Poppins'></input>
                 </div>
                 <div className='mx-12 sm:mx-40 md:mx-16 lg:mx-28 pb-6 relative'>
-                    <label for='password' className='pb-1.5 text-dark text-sm font-Nunito font-black'>Contraseña:</label>
+                    <label className='pb-1.5 text-dark text-sm font-Nunito font-black'>Contraseña:</label>
                     <input value={passwordPerson}  onChange={(e) => {setPasswordPerson(e.target.value)}} type={showPassword ? 'text' : 'password'} required placeholder='∗∗∗∗∗∗∗∗∗∗∗∗∗' className='w-full pl-1 pr-8 py-1 rounded border-2 border-yellow text-dark/50 text-xs font-Poppins'></input>
                         <button className='absolute top-8 right-2 px-1 bg-light' onClick={() => setShowPassword(!showPassword)}>
                             {showPassword ? <AiOutlineEye className='fill-dark hover:fill-red'/> : <AiOutlineEyeInvisible className='fill-dark hover:fill-red'/>}
