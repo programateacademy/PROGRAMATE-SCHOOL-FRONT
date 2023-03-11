436c9d10cbd668d54b75069c3f41b29859cd8c83
danilodiaz <diazdanilo340@gmail.com>
2023-03-11 00:03:44 -0500

    Details

--- src/components/StudentSpace/StudentProgress.jsx
+++ src/components/StudentSpace/StudentProgress.jsx
@@ -7,14 +7,20 @@ const StudentProgress = () => {
         <h1 className="text-start m-6 font-Poppins font-bold">Hola "Nombre estudiante"</h1>
         <h5 className="text-yellow font-Poppins font-bold mx-10">Estas aplicando al programa: Frontend Developer Jr</h5>
         <h6 className="text-start m-4 mb-2 font-bold font-Poppins">Tu progreso</h6>
-        <div className=" flex items-center mx-2 bg-white rounded-full h-2.5 dark:white">
-          <div className="bg-purple w-[50%] h-[7px] rounded-full"></div>
+        <div className=" flex items-center mx-4 bg-white rounded-full h-2.5 dark:white">
+          <div className="bg-purple w-[50%] h-[6px] rounded-full mx-1"></div>
         </div>
         <p className="font-semibold font-Poppins m-4 text-end mt-0">50%</p>
       </section>
       <section className="mx-2 text-dark">
         <h3 className="font-bold font-Poppins mt-4">Tu proceso inicia aquí</h3>
-        <div>
+        <div className="flex justify-between items-center">
+          <div className=" flex items-center mx-4 bg-purple rounded-full h-[150px] w-[10px] relative">
+            <div className="bg-purple w-[50px] h-[50px] rounded-full mx-1 border-2 border-dark absolute flex justify-center items-center text-light font-bold text-3xl">
+              1
+            </div>
+          </div>
+          
           <h2>Fase 1: Registro en la plataforma</h2>
           <p></p>
         </div>
