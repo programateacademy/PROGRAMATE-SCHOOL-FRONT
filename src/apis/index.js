14641ea5c30c7b9593b0fd45fbb63574ad06ea09
Manuel Ricardo <94885870+manuel101284@users.noreply.github.com>
2023-04-03 18:53:39 -0500

    Update index.js

--- src/apis/index.js
+++ src/apis/index.js
@@ -1,5 +1,6 @@
 import axios from 'axios'
 
 export default axios.create({
-    baseURL: "http://localhost:3000/api"
+//     baseURL: "http://localhost:3000/api"
+    baseURL: "https://programate-school-back.vercel.app/"
 });
