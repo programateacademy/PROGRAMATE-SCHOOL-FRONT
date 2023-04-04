7e16e10f3711695da3f8dcc72a8eb312bc248b6f
Prográmate Academy by Educamás <101134847+programateacademy@users.noreply.github.com>
2023-04-03 19:20:16 -0500

    Update index.js

--- src/apis/index.js
+++ src/apis/index.js
@@ -2,5 +2,5 @@ import axios from 'axios'
 
 export default axios.create({
 //     baseURL: "http://localhost:3000/api"
-    baseURL: "https://programate-school-back.vercel.app/"
+    baseURL: "programate-school-back-628dszqun-programateacademy.vercel.app"
 });
